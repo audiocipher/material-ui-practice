7957309440a69bfaf93219bcd44304b24681fee2
aj <audiocipher@gmail.com>
2018-11-21 14:14:11 -0600

    overwriting material-ui using styled components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import { Grid, Paper } from '@material-ui/core';
+
+
+import JssProvider from 'react-jss/lib/JssProvider';
+import { create } from 'jss';
+import { createGenerateClassName, jssPreset } from '@material-ui/core/styles';
+
+const generateClassName = createGenerateClassName();
+const jss = create({
+  ...jssPreset(),
+  // We define a custom insertion point that JSS will look for injecting the styles in the DOM.
+  insertionPoint: document.getElementById('jss-insertion-point'),
+});
+
+
+const StyledPaper = styled(Paper)`
+  background-color: pink;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <JssProvider jss={jss} generateClassName={generateClassName} >
+        <Grid container spacing={24}>
+          <Grid item xs={12}>
+            <StyledPaper>xs=12</StyledPaper>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Paper>xs=12 sm=6</Paper>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Paper>xs=12 sm=6</Paper>
+          </Grid>
+        </Grid>
+      </JssProvider>
     );
   }
 }
